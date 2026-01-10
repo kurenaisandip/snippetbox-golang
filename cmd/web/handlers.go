b0d2ce5e7c5ab8f1830b206eece12f39e294ce42
Sandip <sandip.rai@amniltech.com>
2026-01-10 18:49:13 +0545

    feat: seperated the html in serperat file like in the java jsp

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,22 +9,32 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	// w.Header().Add("Server", "Go")
-	// w.Write([]byte("Hello from Snippetbox"))
-
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	w.Header().Add("Server", "Go")
+	// Initialize a slice containing the paths to the two files. It's important
+	// to note that the file containing our base template must be the *first*
+	// file in the slice.
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+	// Use the template.ParseFiles() function to read the files and store the
+	// templates in a template set. Notice that we use ... to pass the contents
+	// of the files slice as variadic arguments.
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-
-	err = ts.Execute(w, nil)
+	// Use the ExecuteTemplate() method to write the content of the "base"
+	// template as the response body.
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
+
 func snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
