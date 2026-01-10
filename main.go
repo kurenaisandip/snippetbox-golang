c1c9d629e2410e6be7639743068ef0b2476f9178
Sandip <sandip.rai@amniltech.com>
2026-01-10 17:16:46 +0545

    chnaged the write method wit h fmt.fprintf

--- main.go
+++ main.go
@@ -20,8 +20,9 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	msg := fmt.Sprintf("Display a specific snippet with ID %d..", id)
-	w.Write([]byte(msg))
+	// msg := fmt.Sprintf("Display a specific snippet with ID %d..", id)
+	// w.Write([]byte(msg))
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 
 	// w.Write([]byte("Display a specific snippet ....."))
 }
