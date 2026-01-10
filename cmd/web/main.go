b2d1c6afd8377b39e20a8424a45215006229b467
Sandip <sandip.rai@amniltech.com>
2026-01-10 22:29:28 +0545

    refactor: seperated the routes in different file and passed the pointer to the routes in the httpserver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -28,22 +28,11 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
 	// log.Printf("starting server on %s", *addr)
 	// logger.Info("starting server", "addr", *addr)
 	logger.Info("starting server", slog.Any("addr", ":4000"))
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	// err := http.ListenAndServe(":4000", mux)
 	// log.Fatal(err)
 	logger.Error(err.Error())
