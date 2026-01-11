89eb238ba7549790f463808a5b3f5c24cd764449
Sandip <sandip.rai@amniltech.com>
2026-01-11 20:04:49 +0545

    feat: established connection with sql server in my local db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/microsoft/go-mssqldb"
 )
 
 type application struct {
@@ -14,6 +17,11 @@ type application struct {
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String(
+		"dsn",
+		"sqlserver://sa:1@SANDIP-RAI?database=snippetbox&instanceName=SQLEXPRESS",
+		"SQL Server data source name",
+	)
 
 	flag.Parse()
 
@@ -21,6 +29,14 @@ func main() {
 	// logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 	// 	AddSource: true,
 	// })) // this will add the file and line number of the log call
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
 
 	// Initialize a new instance of our application struct, containing the
 	// dependencies (for now, just the structured logger).
@@ -32,9 +48,22 @@ func main() {
 	// logger.Info("starting server", "addr", *addr)
 	logger.Info("starting server", slog.Any("addr", ":4000"))
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	// err := http.ListenAndServe(":4000", mux)
 	// log.Fatal(err)
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("sqlserver", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
